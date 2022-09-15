3fab270c2ad649e58844e166d938ef7c64d51957
sampdubs <sammypw1@gmail.com>
2022-09-15 11:53:23 -0400

    Add all CAN and DIO devices, controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,18 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +25,38 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final WPI_TalonSRX CAN10 = new WPI_TalonSRX(10);
+  private final WPI_VictorSPX CAN11 = new WPI_VictorSPX(11);
+  private final WPI_VictorSPX CAN12 = new WPI_VictorSPX(12);
+
+  private final WPI_TalonSRX CAN20 = new WPI_TalonSRX(20);
+  private final WPI_VictorSPX CAN21 = new WPI_VictorSPX(21);
+  private final WPI_VictorSPX CAN22 = new WPI_VictorSPX(22);
+
+  private final WPI_TalonSRX CAN30 = new WPI_TalonSRX(30);
+  private final WPI_TalonSRX CAN31 = new WPI_TalonSRX(31);
+  private final WPI_TalonSRX CAN32 = new WPI_TalonSRX(32);
+
+  private final WPI_TalonSRX CAN40 = new WPI_TalonSRX(40);
+  private final CANSparkMax CAN41 = new CANSparkMax(41, MotorType.kBrushless);
+
+  private final CANSparkMax CAN50 = new CANSparkMax(50, MotorType.kBrushless);
+  private final CANSparkMax CAN51 = new CANSparkMax(51, MotorType.kBrushless);
+  private final CANSparkMax CAN52 = new CANSparkMax(52, MotorType.kBrushless);
+  private final CANSparkMax CAN53 = new CANSparkMax(53, MotorType.kBrushless);
+
+  private final DigitalInput DIO0 = new DigitalInput(0);
+  private final DigitalInput DIO1 = new DigitalInput(1);
+  private final DigitalInput DIO5 = new DigitalInput(5);
+  private final DigitalInput DIO3 = new DigitalInput(3);
+  private final DigitalInput DIO4 = new DigitalInput(4);
+
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final XboxController driver = new XboxController(0);
+  private final XboxController operator = new XboxController(1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +70,69 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    // Driver buttons
+    // JoystickButton dA = new JoystickButton(driver,
+    // XboxController.Button.kA.value);
+    // JoystickButton dB = new JoystickButton(driver,
+    // XboxController.Button.kB.value);
+    // JoystickButton dX = new JoystickButton(driver,
+    // XboxController.Button.kX.value);
+    // JoystickButton dY = new JoystickButton(driver,
+    // XboxController.Button.kY.value);
+    // JoystickButton dLB = new JoystickButton(driver,
+    // XboxController.Button.kLeftBumper.value);
+    // JoystickButton dRB = new JoystickButton(driver,
+    // XboxController.Button.kRightBumper.value);
+    // XboxControllerButton dLT =
+    // new XboxControllerButton(driver, XboxController.Axis.kLeftTrigger.value);
+    // XboxControllerButton dRT =
+    // new XboxControllerButton(driver, XboxController.Axis.kRightTrigger.value);
+    // JoystickButton dBack = new JoystickButton(driver,
+    // XboxController.Button.kBack.value);
+    // JoystickButton dStart = new JoystickButton(driver,
+    // XboxController.Button.kStart.value);
+    // XboxControllerButton dLeftY =
+    // new XboxControllerButton(driver, XboxController.Axis.kLeftY.value);
+    // XboxControllerButton dLeftX =
+    // new XboxControllerButton(driver, XboxController.Axis.kLeftX.value);
+    // XboxControllerButton dRightY =
+    // new XboxControllerButton(driver, XboxController.Axis.kRightY.value);
+    // XboxControllerButton dRightX =
+    // new XboxControllerButton(driver, XboxController.Axis.kRightX.value);
+
+    // POVTrigger dDPadUp = new POVTrigger(driver, 0);
+    // POVTrigger dDPadRight = new POVTrigger(driver, 90);
+    // POVTrigger dDPadDown = new POVTrigger(driver, 180);
+    // POVTrigger dDPadLeft = new POVTrigger(driver, 270);
+    
+    // Operator buttons
+    // JoystickButton oA = new JoystickButton(operator, XboxController.Button.kA.value);
+    // JoystickButton oB = new JoystickButton(operator, XboxController.Button.kB.value);
+    // JoystickButton oX = new JoystickButton(operator, XboxController.Button.kX.value);
+    // JoystickButton oY = new JoystickButton(operator, XboxController.Button.kY.value);
+    // JoystickButton oLB = new JoystickButton(operator, XboxController.Button.kLeftBumper.value);
+    // JoystickButton oRB = new JoystickButton(operator, XboxController.Button.kRightBumper.value);
+    // XboxControllerButton oLT =
+    // new XboxControllerButton(operator, XboxController.Axis.kLeftTrigger.value);
+    // XboxControllerButton oRT =
+    // new XboxControllerButton(operator, XboxController.Axis.kRightTrigger.value);
+    // JoystickButton oBack = new JoystickButton(operator, XboxController.Button.kBack.value);
+    // JoystickButton oStart = new JoystickButton(operator, XboxController.Button.kStart.value);
+    // XboxControllerButton oLeftY =
+    // new XboxControllerButton(operator, XboxController.Axis.kLeftY.value);
+    // XboxControllerButton oLeftX =
+    // new XboxControllerButton(operator, XboxController.Axis.kLeftX.value);
+    // XboxControllerButton oRightY =
+    // new XboxControllerButton(operator, XboxController.Axis.kRightY.value);
+    // XboxControllerButton oRightX =
+    // new XboxControllerButton(operator, XboxController.Axis.kRightX.value);
+    // POVTrigger oDPadUp = new POVTrigger(operator, 0);
+    // POVTrigger oDPadRight = new POVTrigger(operator, 90);
+    // POVTrigger oDPadDown = new POVTrigger(operator, 180);
+    // POVTrigger oDPadLeft = new POVTrigger(operator, 270);
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
