acdfd082887ec45961c032410a387c803781bf0c
liamgn06 <liamgn06@gmail.com>
2022-09-28 18:28:43 -0400

    first commit daddy

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,11 @@
+// define motors
+// name motors (master, follower)
+// define drive mode options (tank, arcade, split_arcade)
+// define drivetrain
+// add motors to drivetrain
+// set motor inertion (true, false)
+// set motor nuetral mode (brake)
+// tell follower motors to follow masters
+// tell master motor control mode to percent output
+// define controller buttons
+// set base state when robot turns on
